58bb6bc2d452b1f089013ea0d0861eb363b146d8
Chester Holtz <chesterholtz@gmail.com>
2018-03-28 18:55:16 -0500

    textTimed

---
+++ projects/attention-cueing-stroop/jspsych-5.0.3/plugins/jspsych-textTimed.js
@@ -0,0 +1,87 @@
+/**
+ * @name textTimed
+ * @memberof jspsych-text-timed
+ * @function
+ * @param {Object} trial
+ * @param {string} trial.text Text to display
+ * @param {number} [trial.cont_key=[]] Keycodes of key presses that can end trial (as in jsPsych-text plugin: 'mouse' to use mouse, accepts key names too)
+ * @param {number} [trial.timeLeft=null] Specify the lime limit on this trial.
+ *
+ */
+jsPsych.plugins.textTimed = (function() {
+  var plugin = {};
+
+  plugin.trial = function(display_element, trial) {
+
+    trial.cont_key = trial.cont_key || [];
+    trial.timeLeft = trial.timeLeft || null;
+
+    // if any trial variables are functions
+    // this evaluates the function and replaces
+    // it with the output of the function
+    trial = jsPsych.pluginAPI.evaluateFunctionParameters(trial);
+
+    // set the HTML of the display target to replaced_text.
+    display_element.html(trial.text);
+    var timer = Math.round(1000*trial.timeLeft)
+    console.log(timer)
+    if(timer == 0){ timer = 100}
+    //start the timer
+    if(trial.timeLeft){
+      var count = setTimeout(afterTimeOut,timer);
+    }
+
+    var after_response = function(info) {
+      clearTimeout(count);
+      display_element.html(''); // clear the display
+
+      var trialdata = {
+        "rt": info.rt,
+        "key_press": info.key
+      }
+      jsPsych.finishTrial(trialdata);
+    };
+
+    var mouse_listener = function(e) {
+
+      var rt = (new Date()).getTime() - startTime;
+
+      display_element.unbind('click', mouse_listener);
+
+      after_response({
+        key: 'mouse',
+        rt: rt
+      });
+
+    };
+
+    // check if key is 'mouse'
+    if (trial.cont_key == 'mouse') {
+      display_element.click(mouse_listener);
+      var startTime = performance.now();
+    } else {
+      jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.cont_key,
+        rt_method: 'performance',
+        persist: false,
+        allow_held_key: false
+      });
+    };
+
+    function afterTimeOut(){
+      clearTimeout(count);
+      jsPsych.pluginAPI.cancelAllKeyboardResponses();
+      display_element.html(''); // clear the display
+      var rt = (new Date()).getTime() - startTime;
+      var trialdata = {
+        "rt": rt,
+        "key_press": 'timeout'
+      };
+      jsPsych.finishTrial(trialdata);
+    };
+    var startTime = (new Date()).getTime();
+  };
+
+  return plugin;
+})();
